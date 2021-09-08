f1fc2df705e333549416cbac6a31e62c3068d2b5
Abangpa1ace <dyek72731@gmail.com>
2021-09-09 04:22:39 +0900

    전적 컴포넌트 개발중

---
+++ src/components/summoner/GameItem/GameChamp.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {
+  game: GameInfo;
+  winLose: boolean;
+}
+
+const GameChamp: React.FC<Props> = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GameChamp
