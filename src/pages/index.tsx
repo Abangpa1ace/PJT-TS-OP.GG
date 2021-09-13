30b49491d7779ce4b90996091262363edc91bc54
문규찬 <lazychan90@gmail.com>
2021-09-13 12:40:56 +0900

    main page 레이아웃작업중

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react'
-import styled from 'styled-components';
-import Layout from '../components/layout'
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import styled, { css } from 'styled-components';
+import Layout from '../components/layout';
+import theme, { flexCenter } from '../styles/theme';
+import Main from '../components/homepage/Main';
 
 export default function Home() {
-
   return (
     <Layout isBlue>
       <HomePage>
-        <h2>랜딩 페이지!!!</h2>
+        <Main />
       </HomePage>
     </Layout>
-  )
+  );
 }
 
 const HomePage = styled.div`
-
+  ${flexCenter}
 `;
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,9 +2,10 @@ declare global {
   interface ExampleProps {
     accountId: Number;
     name: String;
+    src: string;
   }
 
   type GetSummonerInfo = (name: String) => Promise<ResultType>;
 }
 
-export {}
+export {};
